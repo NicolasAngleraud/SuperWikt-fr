0831d058fe6119286e26d44e8aca6b5c0c5bd60e
Nicolas Angleraud <46026463+NicolasAngleraud@users.noreply.github.com>
2024-02-08 14:08:07 +0100

    Add files via upload

--- lexical_classifier.py
+++ lexical_classifier.py
@@ -72,11 +72,11 @@ def pad_batch(encodings_batch, padding_token_id=2, max_seq_length=100):
 	padding_size = min(padding_size, max_seq_length)
 	for sentence_encoding in encodings_batch:
 		if len(sentence_encoding) < padding_size:
-		    while len(sentence_encoding) < padding_size:
-			sentence_encoding.append(padding_token_id)
+			while len(sentence_encoding) < padding_size:
+				sentence_encoding.append(padding_token_id)
 		else:
-		    while len(sentence_encoding) > padding_size:
-			sentence_encoding.pop(-2)
+			while len(sentence_encoding) > padding_size:
+				sentence_encoding.pop(-2)
 	return torch.tensor(encodings_batch, dtype=torch.long)
 
 
