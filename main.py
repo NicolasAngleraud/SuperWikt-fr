db66ec30bb48f2cf1ab45fd7a499ac8d37cbb93c
Nicolas Angleraud <46026463+NicolasAngleraud@users.noreply.github.com>
2024-02-09 13:05:28 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -70,7 +70,6 @@ if __name__ == '__main__':
 			    dev_data = {}
 			    test_data = {}
 			    
-			    """
 			    print("")
 			    print(f"run {i+1} : lr = {lr}")
 			    print(f"eval set : {eval_prefix}")
@@ -89,8 +88,6 @@ if __name__ == '__main__':
 			    lclf.training(params, train_examples, dev_examples, classifier, DEVICE, dev_data, test_data)
 			    lclf.evaluation(dev_examples, classifier, params, DEVICE,  i+1, f"{eval_prefix}-dev", dev_data)
 			    lclf.evaluation(test_examples, classifier, params, DEVICE, i+1, f"{eval_prefix}-test", test_data)
-			    
-			    """
 			   
 			    sequoia_baseline = lclf.MostFrequentSequoia()
 			    train_baseline = lclf.MostFrequentTrainingData()
@@ -110,14 +107,10 @@ if __name__ == '__main__':
 			    test_data["wiki_baseline"] = wiki_baseline.evaluation(test_examples)
 			    
 			    
-			    print(sequoia_baseline.evaluation(dev_examples))
-			    print(train_baseline.evaluation(dev_examples))
-			    print(wiki_baseline.evaluation(dev_examples))
-			    
 			    df_dev.append(dev_data)
 			    df_test.append(test_data)
 
-"""
+
 	# dev
 	df = pd.DataFrame(df_dev)
 	excel_filename = f'results_{eval_prefix}-dev.xlsx'
@@ -127,4 +120,4 @@ if __name__ == '__main__':
 	df = pd.DataFrame(df_test)
 	excel_filename = 'results_{eval_prefix}-test.xlsx'
 	df.to_excel(excel_filename, index=False)
-"""
+
