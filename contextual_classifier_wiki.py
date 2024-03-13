17dbb20d84c5fd99bad03c8b67b232329fc6035c
Nicolas Angleraud <46026463+NicolasAngleraud@users.noreply.github.com>
2024-03-13 23:19:20 +0100

    Add files via upload

--- contextual_classifier_wiki.py
+++ contextual_classifier_wiki.py
@@ -120,7 +120,7 @@ def encoded_definitions(datafile, nlp, set_, max_length=100):
 	tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 	lemmas = df_senses[df_senses['set']==set_]['lemma'].tolist()
 	
-	definitions = df_senses[df_senses['set']==set_]['definition'].tolist()[16:]
+	definitions = df_senses[df_senses['set']==set_]['definition'].tolist()[:16]
 	# examples = [example.replace('{ { exemple|lang = fr|', '').replace('{ { exemple|', '').replace('{ { exemple', '').replace('<br', '').replace('lang = fr', '') for example in examples]
 	
 	definitions = [ [lemma]+[' : ']+[token.text for token in nlp(x)] for x, lemma in zip(definitions, lemmas) ]
@@ -142,7 +142,7 @@ def encoded_definitions(datafile, nlp, set_, max_length=100):
 	bert_input, index_map = add_special_tokens_batch(bert_input_raw, index_map_raw, cls_id=0, sep_id=1)
 	supersenses_encoded = [supersense2i[supersense] for supersense in supersenses]
 
-	return bert_input[:16], tg_wrks[:16], index_map[:16], supersenses_encoded[16:], senses_ids[16:], lemmas[16:]
+	return bert_input[:16], tg_wrks[:16], index_map[:16], supersenses_encoded[:16], senses_ids[:16], lemmas[:16]
 
 
 
@@ -185,11 +185,11 @@ class SupersenseTagger(nn.Module):
 		# bert_word_embeddings = bert_tok_embeddings.new_zeros((batch_size, max_length, bert_emb_size)).to(self.device) # [batch_size, max_length, bert_emb_size]
 		# bert_word_embeddings = torch_scatter.scatter_mean(bert_tok_embeddings, X_idxmap, out=bert_word_embeddings, dim=1) # [batch_size, max_length, bert_emb_size]
 		# bert_target_word_embeddings = bert_word_embeddings[torch.arange(bert_word_embeddings.size(0)), X_rank] # [batch_size, bert_emb_size]
-		print(f"shape before = {bert_tok_embeddings.shape}")
+		print(f"shape before = {bert_tok_embeddings}")
 		
 		bert_target_word_embeddings = bert_tok_embeddings[:,X_rank,:]
 		
-		print(f"shape after = {bert_target_word_embeddings.shape}")
+		print(f"shape after = {bert_target_word_embeddings}")
 		
 		out = self.linear_1(bert_target_word_embeddings) # SHAPE [len(definitions), hidden_layer_size]
 		
