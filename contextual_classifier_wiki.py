f4f4cde1a96143c43fa0987ddf995bdb6db53398
Nicolas Angleraud <46026463+NicolasAngleraud@users.noreply.github.com>
2024-03-13 16:42:43 +0100

    Add files via upload

--- contextual_classifier_wiki.py
+++ contextual_classifier_wiki.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch_scatter import scatter_mean
+# from torch_scatter import scatter_mean
 import sacremoses
 from random import shuffle
 import numpy as np
