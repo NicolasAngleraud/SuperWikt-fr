0b9ca2f48f06eb63e8f3d382333e307b0c448300
NicolasAngleraud <nicolas.angleraud@gmail.com>
2024-04-16 14:00:17 +0200

    Sense encoder

--- dataEncoder.py
+++ dataEncoder.py
@@ -298,11 +298,9 @@ class senseEncoder(Encoder):
 			lemma = df_definitions[df_definitions['sense_id'] == sense_id]["lemma"].iloc[0]
 			supersense = df_definitions[df_definitions['sense_id'] == sense_id]["supersense"].iloc[0]
 			
-			supersenses_encoded = supersense2i[supersense]
+			definition_with_lemma_encoded = tokenizer.encode(text=f"{lemma.replace('_',' ')} : {definition}", add_special_tokens=True)
 			
-			definitions_with_lemma_encoded = [tokenizer.encode(text=f"{lemma.replace('_',' ')} : {definition}", add_special_tokens=True) for definition, lemma in zip(definitions, lemmas)]
-			
-			definitions_without_lemma_encoded = [tokenizer.encode(text=definition, add_special_tokens=True) for definition, lemma in zip(definitions, lemmas)]
+			definition_without_lemma_encoded = tokenizer.encode(text=definition, add_special_tokens=True)
 			
 			examples = df_examples[df_examples['sense_id'] == sense_id]['example'].tolist()
 			word_ranks = df_examples[df_examples['sense_id'] == sense_id]['word_rank'].tolist()
@@ -318,11 +316,10 @@ class senseEncoder(Encoder):
 			bert_input_raw = [ flatten_list(sent) for sent in sents_encoded ]
 			bert_input_examples, tg_trks_examples = self.add_special_tokens(bert_input_raw, tg_trks, cls_id=0, sep_id=1)
 			
-			definitions_with_lemma_encoded = torch.tensor(definitions_with_lemma_encoded).to(device)
-			definitions_without_lemma_encoded = torch.tensor(definitions_without_lemma_encoded).to(device)
-			supersense_encoded = torch.tensor(supersense_encoded).to(device)
+			definition_with_lemma_encoded = torch.tensor(definition_with_lemma_encoded).to(device)
+			definition_without_lemma_encoded = torch.tensor(definition_without_lemma_encoded).to(device)
 			tg_trks_examples = torch.tensor(tg_trks_examples).to(device)
 			for bert_input in bert_input_examples: bert_input = torch.tensor(bert_input).to(device)
 			
-			yield definitions_with_lemma_encoded, definitions_without_lemma_encoded, bert_input_examples, tg_trks_examples, supersense_encoded, sense_id, lemma
+			yield definition_with_lemma_encoded, definition_without_lemma_encoded, bert_input_examples, tg_trks_examples, supersense, sense_id, lemma
 		
