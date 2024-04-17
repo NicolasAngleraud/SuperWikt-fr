c1df9b8966fbc25b2014e30b039b8d4f8bbdef66
NicolasAngleraud <nicolas.angleraud@gmail.com>
2024-04-17 06:47:24 +0200

    Correct lexical classifier

--- lexicalClf.py
+++ lexicalClf.py
@@ -227,7 +227,7 @@ class monoRankClf(nn.Module):
 				
 				pred = [SUPERSENSES[i] for i in predicted_indices]
 				gold = [SUPERSENSES[i] for i in b_supersenses_encoded.tolist()]
-				definitions = [self.tokenizer.decode(token_ids.tolist()) for token_ids in b_def_encoded]
+				definitions = [self.tokenizer.decode(token_ids.tolist(), skip_special_tokens=True) for token_ids in b_def_encoded]
 				
 				predictions['lemma'].extend(b_lemmas)
 				predictions['sense_id'].extend(b_senses_ids)
@@ -431,7 +431,7 @@ class multiRankClf(nn.Module):
 				
 				pred = [SUPERSENSES[i] for i in predicted_indices]
 				gold = [SUPERSENSES[i] for i in b_supersenses_encoded.tolist()]
-				sentences = [self.tokenizer.decode(token_ids.tolist()) for token_ids in b_def_encoded]
+				sentences = [self.tokenizer.decode(token_ids.tolist(), skip_special_tokens=True) for token_ids in b_def_encoded]
 				
 				predictions['lemma'].extend(b_lemmas)
 				predictions['sense_id'].extend(b_senses_ids)
