45571b75feacf74093146c844b917e0ed5d0d12f
NicolasAngleraud <nicolas.angleraud@gmail.com>
2024-04-26 20:38:37 +0200

    TEST LLAMA3

--- test_llama3.py
+++ test_llama3.py
@@ -1,8 +1,31 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+"""
 # Load tokenizer and model
 tokenizer = AutoTokenizer.from_pretrained("lightblue/suzume-llama-3-8B-multilingual")
 model = AutoModelForCausalLM.from_pretrained("lightblue/suzume-llama-3-8B-multilingual")
+
+# Define prompt
+prompt = "'chien : individu de la race des canidés'. Donne un seul mot correspondant à la classe sémantique de cette définition entre 'person' et 'animal'."
+
+# Tokenize prompt
+input_ids = tokenizer.encode(prompt, return_tensors="pt")
+
+# Generate text based on prompt
+output = model.generate(input_ids, max_length=100, num_return_sequences=1, temperature=0.7)
+
+# Decode generated output
+generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+
+print("Generated Text:")
+print(generated_text)
+"""
+
+
+
+# Load tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B")
 
 # Define prompt
 prompt = "'chien : individu de la race des canidés'. Donne un seul mot correspondant à la classe sémantique de cette définition entre 'person' et 'animal'."
