1bf01e8f6e708956fe4b6561f8e3f2d0b166c2b8
NicolasAngleraud <nicolas.angleraud@gmail.com>
2024-04-26 15:16:02 +0200

    TEST LLAMA3

--- test_llama3.py
+++ test_llama3.py
@@ -14,13 +14,13 @@ module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
 
 
-
+"""
 model = Llama.build(ckpt_dir="../Meta-Llama-3-8B",
 					tokenizer_path="../Meta-Llama-3-8B/tokenizer.model",
 					max_seq_length=100,
 					max_batch_size=2,
 		    		)
-
+"""
 
 
 
