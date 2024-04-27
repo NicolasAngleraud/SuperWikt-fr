e94a8e4fac100e0643fce2cc64d9934a957a01f7
NicolasAngleraud <nicolas.angleraud@gmail.com>
2024-04-28 00:12:09 +0200

    TEST LLAMA3

--- test_llama3.py
+++ test_llama3.py
@@ -15,30 +15,20 @@ model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruc
 
 definition = "Auteur de lettres ou de coups de téléphone anonymes.  "
 
-prompt = """<s>[INST] <<SYS>>
-Tu es un annotateur en sémantique lexciale qui doit attribuer à une définition la classe sémantique qui correspond le plus à ce qui est décrit par la définition. Le choix des classes est restreint aux quatre classes suivantes: person, animal, mineral, plant.
+prompt = """Tu es un annotateur en sémantique lexciale qui doit attribuer à une définition la classe sémantique qui correspond le plus à ce qui est décrit par la définition. Le choix des classes est restreint aux quatre classes suivantes: person, animal, mineral, plant.
 
+Répond au format de la manière suivante, en ne donnant aucun autre information ou mot: {"definition": definition, "classe sémantique": classe sémantique}
 
-Répond au format JSON de la manière suivante: {{"definition": definition, "classe sémantique": classe sémantique}}
-<</SYS>>
+Voici quelques exemples:
+{"définition": "riz : Céréale que l’on cultive dans les terres humides et marécageuses des pays chauds.", "classe sémantique": "plant"},
+{"définition": "oisillon : Petit oiseau.", "classe sémantique": "animal"},
+{"définition": "grand-mère : Mère du père (grand-mère paternelle) ou de la mère (grand-mère maternelle) d’une personne.", "classe sémantique": "person"}
 
-
-Quelle est la classe sémantique des définitions suivantes ? [/INST]
-[
-{{"définition": "riz : Céréale que l’on cultive dans les terres humides et marécageuses des pays chauds.", "classe sémantique": "plant"}},
-{{"définition": "oisillon : Petit oiseau.", "classe sémantique": "animal"}},
-{{"définition": "grand-mère : Mère du père (grand-mère paternelle) ou de la mère (grand-mère maternelle) d’une personne.", "classe sémantique": "person"}},
-] </s>
-
-
-<s>[INST]  
-Quelle est la classe sémantique de la définition suivante ? Répond au format JSON de la manière suivante: {{"definition": definition, "classe sémantique": classe sémantique}}
-Définition: {BODY}
-[/INST]""".format(BODY=definition)
+{"définition: {BODY}", "classe sémantique": """.format(BODY=definition)
 
 
 inputs = tokenizer(prompt, return_tensors="pt")
-output = model.generate(**inputs, max_length=inputs.input_ids.size(1) + 100, num_return_sequences=1, temperature=0.2)
+output = model.generate(**inputs, max_length=inputs.input_ids.size(1) + 50, num_return_sequences=1, temperature=0.)
 
 generated_classification = tokenizer.decode(output[0], skip_special_tokens=True)
 
