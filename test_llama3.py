add00fa671fb82b012a2f1f65c8c40dd491e77af
NicolasAngleraud <nicolas.angleraud@gmail.com>
2024-04-27 12:49:11 +0200

    TEST LLAMA3

--- test_llama3.py
+++ test_llama3.py
@@ -1,34 +1,41 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-"""
-# Load tokenizer and model
-tokenizer = AutoTokenizer.from_pretrained("lightblue/suzume-llama-3-8B-multilingual")
-model = AutoModelForCausalLM.from_pretrained("lightblue/suzume-llama-3-8B-multilingual")
 
-# Define prompt
-prompt = "'chien : individu de la race des canidés'. Donne un seul mot correspondant à la classe sémantique de cette définition entre 'person' et 'animal'."
+## MODELS
+#lightblue/suzume-llama-3-8B-multilingual
+#meta-llama/Meta-Llama-3-8B
+#meta-llama/Meta-Llama-3-8B-Instruct
 
-# Tokenize prompt
-input_ids = tokenizer.encode(prompt, return_tensors="pt")
-
-# Generate text based on prompt
-output = model.generate(input_ids, max_length=100, num_return_sequences=1, temperature=0.7)
-
-# Decode generated output
-generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-
-print("Generated Text:")
-print(generated_text)
-"""
 
 API_TOKEN = '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'
 
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct", use_auth_token=API_TOKEN)
 model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct", use_auth_token=API_TOKEN)
 
-prompt = """Définition: Chien qui vit en meute dans les montages. Classe sémantique: Animal.
-			Définition: Sport d'endurance. Classe sémantique: Act.
-			Définition: Végétal de couleur sombre. Classe sémantique: """
+
+definition = "chien : Mammifère carnivore de la famille des Canidés, apparenté au loup (dont il est considéré comme une sous-espèce) et domestiqué par l’être humain, existant sous de nombreuses races aux morphologies variables, de nom scientifique 'Canis lupus familiaris'."
+
+prompt = """<s>[INST] <<SYS>>
+Tu es un annotateur en sémantique lexciale qui doit attribuer à une définition la classe sémantique qui correspond le plus à ce qui est décrit par la définition. Le choix des classes est restreint aux quatre classes suivantes: person, animal, mineral, plant.
+
+
+Répond au format JSON de la manière suivante: {{"definition": definition, "classe sémantique": classe sémantique}}
+<</SYS>>
+
+
+Quelle est la classe sémantique des définitions suivantes ? [/INST]
+[
+{{"définition": "riz : Céréale que l’on cultive dans les terres humides et marécageuses des pays chauds.", "classe sémantique": "plant"}},
+{{"définition": "oisillon : Petit oiseau.", "classe sémantique": "animal"}},
+{{"définition": "grand-mère : Mère du père (grand-mère paternelle) ou de la mère (grand-mère maternelle) d’une personne.", "classe sémantique": "person"}},
+] </s>
+
+
+<s>[INST]  
+Quelle est la classe sémantique de la définition suivante ? Répond au format JSON de la manière suivante: {{"definition": definition, "classe sémantique": classe sémantique}}
+Définition: {BODY}
+[/INST]""".format(BODY=definition)
+
 
 inputs = tokenizer(prompt, return_tensors="pt")
 output = model.generate(**inputs, max_length=inputs.input_ids.size(1) + 50, num_return_sequences=1, temperature=0.8)
