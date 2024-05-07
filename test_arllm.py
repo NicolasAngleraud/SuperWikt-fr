5e8ed9845fb482d58d84b3d0eaca335c83b8013a
NicolasAngleraud <nicolas.angleraud@gmail.com>
2024-05-07 05:10:24 +0200

    TEST AUTOREGRESSIVE LLMs

--- test_arllm.py
+++ test_arllm.py
@@ -2,6 +2,8 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 import torch.nn as nn
 import argparse
+import dataEncoder as data
+import random
 
 
 ## MODELS
@@ -9,6 +11,21 @@ import argparse
 #meta-llama/Meta-Llama-3-8B
 #meta-llama/Meta-Llama-3-8B-Instruct
 #mistralai/Mistral-7B-Instruct-v0.2
+
+
+SUPERSENSES = ['act', 'animal', 'artifact', 'attribute', 'body', 'cognition',
+               'communication', 'event', 'feeling', 'food', 'institution', 'act*cognition',
+               'object', 'possession', 'person', 'phenomenon', 'plant', 'artifact*cognition',
+               'quantity', 'relation', 'state', 'substance', 'time', 'groupxperson']
+
+HYPERSENSES = {"dynamic_situation": ["act", "event", "phenomenon"],
+               "stative_situation": ["attribute", "state", "feeling", "relation"],
+               "animate_entity": ["animal", "person"],
+               "inanimate_entity": ["artifact", "food", "body", "object", "plant", "substance"],
+               "informational_object": ["cognition", "communication"],
+               "quantification": ["quantity", "part", "group"],
+               "other": ["institution", "possession", "time"]
+               }
 
 
 API_TOKEN = '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'
@@ -32,19 +49,38 @@ if __name__ == '__main__':
 	if device_id == "cpu": DEVICE = "cpu"
 	else:
 		if torch.cuda.is_available(): DEVICE = torch.device("cuda:" + args.device_id)
-
-	def_ = "Mammifère domestique, ongulé de l’ordre des suidés ; porc."
-
-	prompt = """<s>[INST]Choisis la classe sémantique décrivant le mieux la définition suivante parmi les vingt quatre classes suivantes: act, animal, artifact, attribute, body, cognition, communication, event, feeling, food, institution, act*cognition, object, possession, person, phenomenon, plant, artifact*cognition, quantity, relation, state, substance, time, groupxperson. Donne simplement en réponse la classe choisie après 'classe sémantique: ' et ne rajoute aucune autre information. [/INST] </s>
-	définition: {BODY} --> classe sémantique: """.format(BODY=def_)
 	
 	tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B", use_auth_token=API_TOKEN)
 	model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B", use_auth_token=API_TOKEN).to(DEVICE)
+	
+	df_definitions = pd.read_excel(args.data_file, sheet_name='senses', engine='openpyxl')
+	df_definitions = self.df_definitions[self.df_definitions['supersense'].isin(SUPERSENSES)]
+	df_definitions = self.df_definitions[(self.df_definitions['definition'] != "") & (self.df_definitions['definition'].notna())]
+	df_definitions['lemma'] = self.df_definitions['lemma'].str.replace('_', ' ')
+	
+	eval_df = []
+	
+	num_indices = 25
+	indices = random.sample(range(len(df)), num_indices)
+	for index in indices:
+		row = df_definitions.iloc[index]
+		definiton = row["definition"]
+		sense_id = row["sense_id"]
+		dataset = row["set"]
+		lemma = row["lemma"]
+		gold = row["supersense"]
 
-	inputs = tokenizer(prompt, return_tensors="pt").to(DEVICE)
-	output = model.generate(**inputs, max_length=inputs.input_ids.size(1) + 50, num_return_sequences=1, temperature=0.2)
+		prompt = """<s>[INST]Choisis la classe sémantique décrivant le mieux la définition suivante parmi les vingt quatre classes suivantes: act, animal, artifact, attribute, body, cognition, communication, event, feeling, food, institution, act*cognition, object, possession, person, phenomenon, plant, artifact*cognition, quantity, relation, state, substance, time, groupxperson. Donne simplement en réponse la classe choisie après 'classe sémantique: ' et ne rajoute aucune autre information. [/INST] </s>
+		définition: {BODY} --> classe sémantique: """.format(BODY=definiton)
+		
+		inputs = tokenizer(prompt, return_tensors="pt").to(DEVICE)
+		output = model.generate(**inputs, max_length=inputs.input_ids.size(1) + 50, num_return_sequences=1, temperature=0.2)
 
-	generated_classification = tokenizer.decode(output[0], skip_special_tokens=True)
+		generated_classification = tokenizer.decode(output[0], skip_special_tokens=True)
 
-
-	print("Generated Classification:", generated_classification)
+		# print("Generated Classification:", generated_classification)
+		
+		eval_df.append({"lemma": lemma, "sense_id": sense_id, "set": dataset, "definition": definition, "gold": supersense, "prompt":prompt,"answer":generated_classification})
+		
+	eval_df = pd.DataFrame(eval_df)
+	eval_df.to_excel("./eval_sample_def_zero_shot_prompting_llama3.xlsx", index=False)
