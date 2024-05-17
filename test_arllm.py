dc604f71a5fcf488f1d7cfe90ae1dfca8ec62f21
NicolasAngleraud <nicolas.angleraud@gmail.com>
2024-05-17 18:10:03 +0200

    TEST PEFT

--- test_arllm.py
+++ test_arllm.py
@@ -89,16 +89,16 @@ if __name__ == '__main__':
 	tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=API_TOKEN, add_eos_token=True)
 	tokenizer.pad_token_id = tokenizer.eos_token_id
 
-	bnb_config = BitsAndBytesConfig(
-									load_in_4bit= True,
-									bnb_4bit_quant_type= "nf4",
-									bnb_4bit_compute_dtype= torch.bfloat16,
-									bnb_4bit_use_double_quant= False)
+	#bnb_config = BitsAndBytesConfig(
+	#								load_in_4bit= True,
+	#								bnb_4bit_quant_type= "nf4",
+	#								bnb_4bit_compute_dtype= torch.bfloat16,
+	#								bnb_4bit_use_double_quant= False)
 	
 	model = AutoModelForCausalLM.from_pretrained(
 												model_name, 
 												use_auth_token=API_TOKEN,
-												quantization_config=bnb_config,
+												#quantization_config=bnb_config,
 												torch_dtype=torch.bfloat16)
 	model.to(DEVICE)
 	
