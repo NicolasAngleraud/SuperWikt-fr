36f2557c2a0de13b2f74c826408b1b1071ae0948
NicolasAngleraud <nicolas.angleraud@gmail.com>
2024-05-27 09:31:47 +0200

    lex clf

--- lexicalClf.py
+++ lexicalClf.py
@@ -594,6 +594,7 @@ class lexicalClf_V1():
 				
 				pred = SUPERSENSES[predicted_index]
 				gold = supersense
+				print(definition_with_lemma_encoded)
 				sentence = self.tokenizer.decode(definition_with_lemma_encoded, skip_special_tokens=True)
 				
 				predictions['lemma'].append(lemma)
