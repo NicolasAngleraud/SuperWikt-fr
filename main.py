8ea1a76d85c43da03c3f54d431142ceb7c2afffc
NicolasAngleraud <nicolas.angleraud@gmail.com>
2024-06-18 00:57:20 +0200

    baseline

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
 	"batch_size": 16,
 	"hidden_layer_size": 768,
 	"patience": 2,
-	"lr": 0.000005,
+	"lr": 0.001,
 	"weight_decay": 0.001,
 	"frozen": True,
 	"max_seq_length": 100
@@ -111,7 +111,51 @@ if __name__ == '__main__':
 	
 	
 	tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+
+	print('ENCODING DATA...')
+	train_definitions_encoder = data.definitionEncoder(args.data_file, "train", tokenizer, use_sample=False)
+	train_definitions_encoder.encode()
+	freq_dev_definitions_encoder = data.definitionEncoder(args.data_file, "freq-dev", tokenizer, use_sample=False)
+	freq_dev_definitions_encoder.encode()
+	rand_dev_definitions_encoder = data.definitionEncoder(args.data_file, "rand-dev", tokenizer, use_sample=False)
+	rand_dev_definitions_encoder.encode()
+	print('DATA ENCODED.')
 	
+	
+	def_clf = clf.monoRankClf(params_def, DEVICE, use_lemma=False, bert_model_name=MODEL_NAME)
+	def_clf.train_clf(train_definitions_encoder, freq_dev_definitions_encoder, rand_dev_definitions_encoder, def_clf_file)
+	def_clf = clf.monoRankClf(params_def, DEVICE, use_lemma=False, bert_model_name=MODEL_NAME)
+	def_clf.load_clf(def_clf_file)
+	
+	train_accuracy = def_clf.evaluate(train_definitions_encoder)
+	freq_dev_accuracy = def_clf.evaluate(freq_dev_definitions_encoder)
+	rand_dev_accuracy = def_clf.evaluate(rand_dev_definitions_encoder)
+	
+	freq_dev_predictions = def_clf.predict(freq_dev_definitions_encoder)
+	rand_dev_predictions = def_clf.predict(rand_dev_definitions_encoder)
+	
+	print("train def accurcay = ", percentage(train_accuracy))
+	print("freq dev def accurcay = ", percentage(freq_dev_accuracy))
+	print("rand dev def accurcay = ", percentage(rand_dev_accuracy))
+	print()
+	
+	freq_dev_def_df = pd.DataFrame(freq_dev_predictions)
+	freq_dev_def_df.to_excel('./baseline_freq_dev.xlsx', index=False)
+	
+	rand_dev_def_df = pd.DataFrame(rand_dev_predictions)
+	rand_dev_def_df.to_excel('./baseline_rand_dev.xlsx', index=False)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	##################################################################################################
+	"""
 	freq_test_sense_encoder = data.senseEncoder(args.data_file, "freq-test", tokenizer, use_sample=False)
 	rand_test_sense_encoder = data.senseEncoder(args.data_file, "rand-test", tokenizer, use_sample=False)
 	
@@ -129,7 +173,7 @@ if __name__ == '__main__':
 	
 	rand_test_def_ex_df = pd.DataFrame(rand_test_predictions)
 	rand_test_def_ex_df.to_excel('./sense_rand_test_predictions.xlsx', index=False)
-	
+	"""
 	
 	
 	
