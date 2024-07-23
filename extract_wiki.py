efd65d067421e268b339f7f101b8aded043cfa18
NicolasAngleraud <nicolas.angleraud@gmail.com>
2024-07-23 17:07:31 +0200

    pipelines

--- extract_wiki.py
+++ extract_wiki.py
@@ -2,11 +2,68 @@ import pandas as pd
 import argparse
 
 
-def extract_wiktionary(input_file, output_file):
+
+
+allowed_relations = ["rdf:type",
+                     "lexinfo:partOfSpeech",
+                     "ontolex:canonicalForm",
+                     "ontolex:sense",
+                     "dbnary:describes",
+                     "dbnary:synonym",
+                     "lexinfo:gender",
+                     "skos:definition",
+                     "skos:example",
+                     "rdfs:label",
+                     "dbnary:partOfSpeech"]
+
+allowed_rdf_types = ["ontolex:LexicalSense",
+                     "ontolex:Form",
+                     "ontolex:LexicalEntry",
+                     "dbnary:Page",
+                     "ontolex:Word",
+                     "ontolex:MultiWordExpression"]
+
+allowed_categories = ["lexinfo:noun", '"-nom-"']
+
+labels_to_ignore = ["vieilli", "archaïque", "désuet", "archaïque, orthographe d’avant 1835"]
+
+lang = "fra"
+
+
+def extract_wiki_paragraphs(input_file):
+	
+	paragraphs = []
 	with open(input_file, 'r', encoding='utf-8') as file:
+		paragraph = []
+		
 		for i, line in enumerate(file):
-		    print(line.strip())
+		
+			if line.startswith('@'): continue
+			
+			elif not line.strip() and len(paragraph)>0:
+				paragraphs.append(paragraph)
+				paragraph = []
+				
+		    else:
+		    	line = line.strip()
+		    	paragraph.append(line)
+		    	
 		    if i >= 76: break
+		    
+	for par in paragraphs:
+		for l in par:
+			print(l)
+		print()
+		
+	return paragraphs
+	
+
+def filter_paragraphs(paragraphs):
+	return paragraphs
+
+
+def parse_paragraphs(paragraphs_list, output_file):
+	pass
 
 
 def main():
@@ -16,7 +73,11 @@ def main():
 
     args = parser.parse_args()
 
-    extract_wiktionary(args.input, args.output)
+    paragraphs = extract_wiki_paragraphs(args.input)
+    
+    paragraphs = filter_paragraphs(paragraphs)
+    
+    parse_paragraphs(paragraphs, args.output)
 
 if __name__ == "__main__":
     main()
