ddd8f533ae156ae5f552aa5098bb89bbcbed5986
NicolasAngleraud <nicolas.angleraud@gmail.com>
2024-08-14 13:15:44 +0200

    llm supersense clf experiments

--- llm_supersense.py
+++ llm_supersense.py
@@ -2,6 +2,32 @@ import pandas as pd
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
+
+
+def pretty_print(prompt, generated_text):
+	print()
+	print()
+	print("*********************************************************")
+	print("PROMPT")
+	print("*********************************************************")
+	print()
+	
+	print(prompt)
+	
+	print()
+	print("*********************************************************")
+	print("ANSWER")
+	print("*********************************************************")
+	print()
+	
+	print(generated_text[len(prompt):])
+	
+	print()
+	print()
+	print("*********************************************************")
+
+
+
 model_name = "meta-llama/Meta-Llama-3.1-8B-Instruct"
 token = "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"
 
@@ -12,7 +38,7 @@ if tokenizer.pad_token_id is None:
     tokenizer.pad_token_id = tokenizer.eos_token_id
 
 prompt = """Quelle est le type sémantique de l'entité associé à la définition suivante ?
-Types possibles: Animal, Plant, Act, Cognition.
+Types possibles: Animal, Plant, Act, Cognition, Feeling.
 Définition: Sentiment éprouvé lors d'une situation difficile.
 Type sémantique:
 """
@@ -28,12 +54,5 @@ outputs = model.generate(
 
 generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
+pretty_print(prompt, generated_text)
 
-print()
-print("*********************************************************")
-print()
-
-print(generated_text)
-
-print()
-print("*********************************************************")
