5ba431aa4cef0a443029cfa3eec88bde51d4376c
NicolasAngleraud <nicolas.angleraud@gmail.com>
2024-08-25 20:09:06 +0200

    llm supersense clf experiments

--- llm_supersense.py
+++ llm_supersense.py
@@ -386,16 +386,16 @@ if __name__ == '__main__':
 	supersenses_tok = [tokenizer.encode(supersense, add_special_tokens=False)[0] for supersense in SUPERSENSES]
 	id2ss = {id_tok: SUPERSENSES[i] for i, id_tok in enumerate(supersenses_tok)}
 	
-	'''
+	
 	freq_dev_encoder = promptEncoder(data_file=data_file, tokenizer=tokenizer, device=device, dataset='freq-dev')
-	freq_dev_encoder.encode()
+	freq_dev_encoder.encode(use_lemma=False)
 	accuracy_freq_dev = model.evaluate(freq_dev_encoder, supersenses_tok)
 	print("FREQ DEV ACCURACY = ", accuracy_freq_dev)
-	'''
 	
+	'''
 	rand_dev_encoder = promptEncoder(data_file=data_file, tokenizer=tokenizer, device=device, dataset='rand-dev')
-	rand_dev_encoder.encode()
+	rand_dev_encoder.encode(use_lemma=False)
 	accuracy_rand_dev = model.evaluate(rand_dev_encoder, supersenses_tok)
 	print("RAND DEV ACCURACY = ", accuracy_rand_dev)	
-	
+	'''
 	print("Process done.")
